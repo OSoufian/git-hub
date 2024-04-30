ccea499837a7f45497a7b70659ab61d06a004afb
OSoufian <osoufian@hotmail.fr>
2024-04-30 18:30:20 +0200

    Lister en fonction d'un username

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,23 +1,26 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import axios from 'axios';
 import React, { useEffect, useState } from 'react';
-
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonInput, IonButton } from '@ionic/react';
+import axios from 'axios';
 import './Tab1.css';
 
 const Tab1: React.FC = () => {
   const [repositories, setRepositories] = useState<any[]>([]);
+  const [username, setUsername] = useState('');
+  const [error, setError] = useState<string>('');
+
+  const fetchRepositories = async () => {
+    try {
+      const response = await axios.get(`https://api.github.com/users/${username}/repos`);
+      setRepositories(response.data);
+      setError('');
+    } catch (error) {
+      console.error('Error fetching repositories:', error);
+      setError('Error fetching repositories');
+      setRepositories([]);
+    }
+  };
 
   useEffect(() => {
-    const fetchRepositories = async () => {
-      try {
-        const response = await axios.get('https://api.github.com/users/OSoufian/repos');
-        setRepositories(response.data);
-      } catch (error) {
-        console.error('Error fetching repositories:', error);
-      }
-    };
-
     fetchRepositories();
   }, []);
 
@@ -25,22 +28,29 @@ const Tab1: React.FC = () => {
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 1</IonTitle>
+          <IonTitle>GitHub Repositories</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Tab 1</IonTitle>
-          </IonToolbar>
-        </IonHeader>
         <div className="ion-padding">
-          <h2>Your GitHub Repositories:</h2>
-          <ul>
-            {repositories.map(repo => (
-              <li key={repo.id}>{repo.name}</li>
-            ))}
-          </ul>
+          <IonInput
+            value={username}
+            placeholder="Enter your GitHub username"
+            onIonChange={(e) => setUsername(e.detail.value!)}
+            clearInput
+          />
+          <IonButton onClick={fetchRepositories}>Fetch Repositories</IonButton>
+          {error && <p>{error}</p>}
+          {repositories.length > 0 && (
+            <div>
+              <h2>Your GitHub Repositories:</h2>
+              <ul>
+                {repositories.map(repo => (
+                  <li key={repo.id}>{repo.name}</li>
+                ))}
+              </ul>
+            </div>
+          )}
         </div>
       </IonContent>
     </IonPage>
